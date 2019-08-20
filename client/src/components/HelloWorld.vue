84e6faf27aada8278c6906e803bc1f5e947d378e
Serge <mandapat30@gmail.com>
2019-08-20 15:48:41 -0400

    Intro page completion

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="hello">
     <h3>{{ msg }}</h3>
-    <p class="mb-2">The application is configured and ready to import our components. You will find MDB Vue docs <a href="https://mdbootstrap.com/docs/vue/" target="_blank">here</a>.</p>
+    <p class="mb-1">The application is configured and ready to import our components. You will find MDB Vue docs <a href="https://mdbootstrap.com/docs/vue/" target="_blank">here</a>.</p>
   </div>
 </template>
 
+
+
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your MDB Vue.js App'
+      msg: "Welcome to Your MDB Vue.js App"
     };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
